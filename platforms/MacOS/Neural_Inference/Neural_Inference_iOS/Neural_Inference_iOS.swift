0e5a39a1efd97df00e321e88037805fb0a581c83
Karine Larose <karine-goldsmiths@cgs-macl-29650.campus.goldsmiths.ac.uk>
2022-02-06 02:32:26 +0000

    Added iOS platform for Unity plugin.

---
+++ platforms/MacOS/Neural_Inference/Neural_Inference_iOS/Neural_Inference_iOS.swift
@@ -0,0 +1,11 @@
+//
+//  Neural_Inference_iOS.swift
+//  Neural_Inference_iOS
+//
+//  Created by Karine Larose on 06/02/2022.
+//  Copyright © 2022 Neurotherapeutics. All rights reserved.
+//
+
+class Neural_Inference_iOS {
+
+}
